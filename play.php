f11af67e10a9284c3f9af51eb797d5c0a860a73f
ALU <59906227+ALUTECHNOLOGIES@users.noreply.github.com>
2021-06-30 11:25:21 +0300

    Update play.php

--- play.php
+++ play.php
@@ -1,12 +1,12 @@
 
 <?php
-require_once './vendor/autoload.php';
-use Twilio\TwiML\VoiceResponse;
+//require_once './vendor/autoload.php';
+//use Twilio\TwiML\VoiceResponse;
 
-$response = new VoiceResponse();
-$response->play('https://alutechnologies.github.io/web/diamond.mp3');
+//$response = new VoiceResponse();
+//$response->play('https://alutechnologies.github.io/web/diamond.mp3');
 
-echo $response;
+//echo $response;
 
 header('Content-Type: audio/mpeg');
 echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
